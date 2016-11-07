f85caa06e19d11abd4b5bd0c82b6118b11b5d433
zhaxc <zhaxc0919@aliyun.com>
2016-11-07 09:50:16 +0800

    company

---
+++ extjsMVC/src/main/webapp/event.js
@@ -0,0 +1,12 @@
+/**
+ * Created by java on 2016/11/7.
+ */
+Ext.onReady(function () {
+    Ext.Create("Ext.panel",{
+        html:'面板',
+        renderTo: Ext.getBody(),
+        listeners:{
+            
+        }
+    });
+});
